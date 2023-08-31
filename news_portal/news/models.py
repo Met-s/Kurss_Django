d74b8fa20d0dbe02c24a7fd836dac45f5700ab17
Met-s <den_vo@mail.ru>
2023-08-31 13:15:11 +0300

    Создал скелет приложения

--- news_portal/news/models.py
+++ news_portal/news/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+
+    def update_rating(self):
+        pass
+
+
+class Category(models.Model):
+    pass
+
+
+class Post(models.Model):
+
+    def like(self):
+        pass
+
+    def dislike(self):
+        pass
+
+
+class PostCategory(models.Model):
+    pass
+
+
+class Comment(models.Model):
+
+    def like(self):
+        pass
+
+    def dislike(self):
+        pass
