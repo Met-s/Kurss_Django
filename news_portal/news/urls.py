2a6a1224cda691c5ae575cb2c0ca3cb069cb4115
Met-s <den_vo@mail.ru>
2023-09-19 14:43:14 +0300

    D_4 Добавил страницы редактирования новостей и статей

--- news_portal/news/urls.py
+++ news_portal/news/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import (PostList, PostDetail, NewsCreate, ArticleCreate)
+from .views import (PostList, PostDetail, NewsCreate, ArticleCreate,
+                    NewsUpdate, ArticleUpdate)
 
 
 urlpatterns = [
     path('', PostList.as_view()),
     path('<int:pk>', PostDetail.as_view(), name='post_detail'),
     path('news/create/', NewsCreate.as_view(), name='news_create'),
-    path('articles/create/', ArticleCreate.as_view(), name='article_create')
+    path('articles/create/', ArticleCreate.as_view(), name='article_create'),
+    path('news/<int:pk>/edit/', NewsUpdate.as_view(), name='news_edit'),
+    path('articles/<int:pk>/edit/', ArticleUpdate.as_view(), name='article_edit'),
 ]
--- news_portal/news/views.py
+++ news_portal/news/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from datetime import datetime
 from django.views.generic import (
-    ListView, DetailView, CreateView)
+    ListView, DetailView, CreateView, UpdateView)
 from .models import Post
 from .forms import PostForm
+from django.http import HttpResponse
 
 
 class PostList(ListView):
@@ -48,3 +49,37 @@ class ArticleCreate(CreateView):
         post = form.save(commit=True)
         post.category_type = 'AR'
         return super().form_valid(form)
+
+
+class NewsUpdate(UpdateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'news_edit.html'
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        if post.category_type == 'NW':
+            return super().form_valid(form)
+        else:
+            html = (f"<html><body><h1>"
+                    f"Эта страница для редактирования Новостей.<br>"
+                    f"Для редактирования Статей перейдите по адресу "
+                    f"/article/int:pk/edit </h1></body></html>")
+            return HttpResponse(html)
+
+
+class ArticleUpdate(UpdateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'news_edit.html'
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        if post.category_type == 'AR':
+            return super().form_valid(form)
+        else:
+            html = (f"<html><body><h1>"
+                    f"Эта страница для редактирования Статей.<br>"
+                    f"Для редактирования Новостей перейдите по адресу "
+                    f"/news/int:pk/edit </h1></body></html>")
+            return HttpResponse(html)
