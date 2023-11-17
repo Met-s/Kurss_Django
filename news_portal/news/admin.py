99edd46f1c1a5ba7e83e9dfc30b0710fa2f00deb
Met-s <den_vo@mail.ru>
2023-11-17 14:56:39 +0600

    D_11. Метод вывода, post_category ManyToMany.В Admin панель

--- news_portal/news/admin.py
+++ news_portal/news/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
-from .models import Author, Post, Category
+from .models import Author, Post, Category, PostCategory, Comment, Subscriber
+
+
+class PostAdmin(admin.ModelAdmin):
+
+    list_display = ('post_date', 'category', 'post_author', 'category_type',
+                    'post_title', 'post_rating')
+
+    def category(self, post):
+        """
+        Метод выводит категорию поста, при связи ManyToMany
+        """
+        return ', '.join([_.category_name for _ in post.post_category.all()])
+
 
 admin.site.register(Author)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
 admin.site.register(Category)
+admin.site.register(PostCategory)
+admin.site.register(Comment)
+admin.site.register(Subscriber)
