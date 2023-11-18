99d1c538fa2a92839e8cb99a1c398e2d2c6855fa
Met-s <den_vo@mail.ru>
2023-11-18 12:15:07 +0300

    D_11. Админка фильтра

--- news_portal/news/admin.py
+++ news_portal/news/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
-from .models import Author, Post, Category, PostCategory, Comment, Subscriber
+from .models import Author, Post, Category, Comment, Subscriber
 
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ('post_date', 'category', 'post_author', 'category_type',
                     'post_title', 'post_rating')
+    list_filter = ('post_date', 'post_category__category_name', 'post_author',
+                   'category_type',
+                   'post_rating')
 
     def category(self, post):
         """
@@ -16,6 +19,7 @@ class PostAdmin(admin.ModelAdmin):
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('comment_post', 'comment_user', 'comment_text',
                     'comment_date', 'comment_rating')
+    list_filter = ('comment_user', 'comment_date', 'comment_rating')
 
 
 admin.site.register(Author)
